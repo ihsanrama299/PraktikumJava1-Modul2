2211546f01c6c851fe8fa08800c34cad6e5f071c
ihsanrama299 <ihsanramadhan299@gmail.com>
2021-06-03 17:35:28 +0800

    Initial commit

---
+++ src/model/Mahasiswa.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.Date;
+
+public class Mahasiswa {
+
+    String nama;
+    String npm;
+    Date tanggalLahir;
+
+}
---
+++ src/model/MahasiswaBeraksi.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MahasiswaBeraksi {
+
+    public static void main(String[] args) throws ParseException {
+
+        String polaTanggal = "dd-MM-yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(polaTanggal);
+
+        Mahasiswa mahasiswa = new Mahasiswa();
+        mahasiswa.npm = "196399999";
+        mahasiswa.nama = "Sembilu";
+        mahasiswa.tanggalLahir = simpleDateFormat.parse("09-09-2009");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa.npm);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa.tanggalLahir));
+
+        Mahasiswa mahasiswa2 = new Mahasiswa();
+        mahasiswa2.npm = "19638888";
+        mahasiswa2.nama = "Delapu";
+        mahasiswa2.tanggalLahir = simpleDateFormat.parse("08-08-2008");
+
+        System.out.println("Nama \t\t\t : " + mahasiswa2.nama);
+        System.out.println("NPM \t\t\t : " + mahasiswa2.npm);
+        System.out.println("Tanggal Lahir \t : " + simpleDateFormat.format(mahasiswa2.tanggalLahir));
+
+    }
+
+}
